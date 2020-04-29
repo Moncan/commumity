75ff53b5df91f6d49cab840f5fc631b5b221aa16
Moncan <1060156362@qq.com>
2020-04-29 18:19:29 +0800

    extract github

---
+++ src/main/java/com/comm/community/dto/GithubUser.java
@@ -0,0 +1,31 @@
+package com.comm.community.dto;
+
+public class GithubUser {
+    private Long id;
+    private String name;
+    private String dio;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDio() {
+        return dio;
+    }
+
+    public void setDio(String dio) {
+        this.dio = dio;
+    }
+}

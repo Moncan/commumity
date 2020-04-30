f04905197a39e89a402472acf3221c551e9850ed
Moncan <1060156362@qq.com>
2020-05-01 02:27:13 +0800

    21th learn

--- src/main/java/com/comm/community/controller/IndexController.java
+++ src/main/java/com/comm/community/controller/IndexController.java
@@ -17,18 +17,21 @@ public class IndexController {
     @Autowired
     private UserMapper userMapper;
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user= userMapper.findByToken(token);
-                if (user!=null){
-                    request.getSession().setAttribute("user",user);
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
                 }
-                break;
             }
         }
+
         return "index";
     }
 

091c2e5a84551712b1e20598b610a75911355d7a
Moncan <1060156362@qq.com>
2020-05-24 01:36:50 +0800

    通知功能

--- src/main/java/com/comm/community/controller/ProfileController.java
+++ src/main/java/com/comm/community/controller/ProfileController.java
@@ -1,8 +1,10 @@
 package com.comm.community.controller;
 
+import com.comm.community.dto.NotificationDTO;
 import com.comm.community.dto.PaginationDTO;
 import com.comm.community.mapper.UserMapper;
 import com.comm.community.model.User;
+import com.comm.community.service.NotificationService;
 import com.comm.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.management.Notification;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class ProfileController {
@@ -22,6 +26,9 @@ public class ProfileController {
 
     @Autowired
     QuestionService questionService;
+
+    @Autowired
+    NotificationService notificationService;
 
     @GetMapping("/profile/{action}")
     public String profile(@PathVariable(name="action") String action,
@@ -36,13 +43,14 @@ public class ProfileController {
         if("questions".equals(action)){
             model.addAttribute("section","questions");
             model.addAttribute("sectionName","我的提问");
+            PaginationDTO paginationDTO = questionService.list(user.getId(), page, size);
+            model.addAttribute("pagination",paginationDTO);
         }else if ("replies".equals(action)){
+            PaginationDTO paginationDTO = notificationService.list(user.getId(), page, size);
             model.addAttribute("section","replies");
+            model.addAttribute("pagination",paginationDTO);
             model.addAttribute("sectionName","最新回复");
         }
-
-        PaginationDTO paginationDTO = questionService.list(user.getId(), page, size);
-        model.addAttribute("pagination",paginationDTO);
         return "profile";
     }
 
--- src/main/java/com/comm/community/service/QuestionService.java
+++ src/main/java/com/comm/community/service/QuestionService.java
@@ -60,7 +60,7 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        paginationDTO.setQuestions(questionDTOList);
+        paginationDTO.setData(questionDTOList);
 
         return paginationDTO;
     }
@@ -97,7 +97,7 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        paginationDTO.setQuestions(questionDTOList);
+        paginationDTO.setData(questionDTOList);
 
         return paginationDTO;
     }
